8f7e88ba21762a965d76b75893dcdb0973c6f1ce
vasif123 <vasifaha@code.edu.az>
2022-04-04 00:07:31 +0400

    homeworkvisual

---
+++ classworkvisiol/classworkvisiol/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classworkvisiol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ homework1c-sharp/homework1c-sharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace homework1c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sum();
+        }
+        #region task-1
+        //Daxil Edilmis 3 Dene Ededin Arasinda En Boyuyunu Tapan Alqoritm Qurmaq.
+        static void sum()
+        {
+            Console.WriteLine("deyer daxil et");
+            string num1str = Console.ReadLine();
+            string num2str = Console.ReadLine();
+            string num3str = Console.ReadLine();
+
+            try
+            {
+                int a = int.Parse(num1str);
+                int b = int.Parse(num2str);
+                int c = int.Parse(num3str);
+                if (a > b && a > c)
+                {
+                    Console.WriteLine($"en boyuk {a} dir");
+                }
+                else if (b > c && b > a)
+                {
+                    Console.WriteLine($"en boyuk {b} dir");
+                }
+                else if (c > b && c > a)
+                {
+                    Console.WriteLine($"en boyuk {c} dir");
+                }
+                else
+                {
+                    Console.WriteLine("ededler bir birine beraberdi");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("eded daxil et");
+                //    }
+                #endregion
+
+
+            }
+        }
+    }
+}
